9efbf29af432d0f25b5f739192d75d7f9db3a6ef
kazamihirotugu <monamona.stitch@gmail.com>
2025-12-31 01:39:10 +0900

    2-6

---
+++ 2-6/Task2_6.java
@@ -0,0 +1,41 @@
+/**
+ * Task2_6: 課題内容
+ *
+ * 本課題では、データ型・文字列の使い方を学んでいきましょう。
+ * 問①から問⑧まであります。
+ * 指定された型・値・変数名を守って記述してください。
+ * 問③,⑦については、コメントを入れてください。
+ *
+ */
+public class Task2_6 {
+
+    public static void main(String[] args) {
+
+        // ① int型の hogeInt という名前の変数を宣言し、値「50」で初期化
+        int hogeInt = 50;
+
+        // ② Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
+        String hogeString = "テスト";
+
+        // ③ 下記の処理について、何をしているのかコメントを記入してください
+        // double型の変数 hogeDouble を宣言し、値「0.8」で初期化しています。
+        double hogeDouble = 0.8;
+
+        // ④ 変数 hogeInt を値「100」で上書き
+        hogeInt = 100;
+
+        // ⑤ 変数 hogeString を値「良くできました！」で上書き
+        hogeString = "良くできました！";
+
+        // ⑥ 変数 hogeInt の値を表示
+        System.out.println(hogeInt);
+
+        // ⑦ 下記の処理について、何をしているのかコメントを記入してください
+        // 変数 hogeString の値をコンソールに出力しています。
+        System.out.println(hogeString);
+
+        // ⑧ 変数 hogeDouble の値を表示
+        System.out.println(hogeDouble);
+
+    }
+}
